1cfb31e0582f76b88108c6f18035330665aad753
Pis <piscarefree@gmail.com>
2024-03-16 02:08:07 +0800

    update

--- contracts/SPVVerifier.sol
+++ contracts/SPVVerifier.sol
@@ -1,11 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import {ISPVVerifier} from "./interface/ISPVVerifier.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+interface ISPVVerifier {
+    function verify(
+        bytes32[] memory proof,
+        bytes32 leaf,
+        bytes calldata zkProof
+    ) external;
+
+    function setVerifier(address _verifier) external;
+
+    function spvVerifierImpl() external view returns (address);
+
+    function merkleRoot() external view returns (bytes32);
+
+    function owner() external view returns (address);
+
+    function syncRoot(bytes32 _newRoot) external;
+
+    event VerifySuccess(address indexed verifier, bytes32 proofHash);
+}
 
 contract SPVVerifier is ISPVVerifier {
     error NotOwner();
-    bytes32 public override state;
+    error VerifyError();
+    using MerkleProof for bytes32[];
+    bytes32 public override merkleRoot;
     address public override spvVerifierImpl;
     address public override owner;
 
@@ -20,16 +42,25 @@ contract SPVVerifier is ISPVVerifier {
         _;
     }
 
-    function verify(bytes calldata _proof) external override returns (bool) {
-        (bool success, ) = spvVerifierImpl.call(_proof);
-        return success;
+    function verify(
+        bytes32[] memory proof,
+        bytes32 leaf,
+        bytes calldata zkProof
+    ) external override {
+        bool mptSuccess = proof.verify(merkleRoot, leaf);
+        (bool cirSuccess, ) = spvVerifierImpl.call(zkProof);
+        if (!mptSuccess || !cirSuccess) {
+            revert VerifyError();
+        }
+
+        emit VerifySuccess(msg.sender, leaf);
     }
 
     function setVerifier(address _verifier) public override onlyOwner {
         spvVerifierImpl = _verifier;
     }
 
-    function syncState(bytes32 _newState) external override onlyOwner {
-        state = _newState;
+    function syncRoot(bytes32 _newRoot) external override onlyOwner {
+        merkleRoot = _newRoot;
     }
 }
--- contracts/interface/ISPVVerifier.sol
+++ contracts/interface/ISPVVerifier.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.23;
 
 interface ISPVVerifier {
-    function verify(bytes calldata _proof) external returns (bool);
+    function verify(bytes32[] memory proof, bytes32 leaf) external;
 
     function setVerifier(address _verifier) external;
 
     function spvVerifierImpl() external view returns (address);
 
-    function state() external view returns (bytes32);
+    function merkleRoot() external view returns (bytes32);
 
     function owner() external view returns (address);
 
     function syncState(bytes32 _newState) external;
+
+    event VerifySuccess(address indexed verifier, bytes32 proofHash);
 }
